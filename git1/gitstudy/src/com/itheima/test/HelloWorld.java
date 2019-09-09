03c5544a3369789df007f2e7772a2f08377b35cd
zyz <798290348@qq.com>
2019-09-09 15:49:44 +0800

    第一次提交git1

---
+++ git1/gitstudy/src/com/itheima/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("欢迎来到git1");
+        System.out.println("欢迎来到git2");
+        System.out.println("欢迎来到git3");
+        System.out.println("欢迎来到git4");
+        System.out.println("欢迎来到git5");
+    }
+}
